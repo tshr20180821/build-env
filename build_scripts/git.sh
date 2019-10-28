aa1352b758625db67c0b834e816438a4b9e98e30
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-29 06:25:36 +0900

    Update git.sh

--- build_scripts/git.sh
+++ build_scripts/git.sh
@@ -7,7 +7,7 @@ date
 GIT_VERSION=v2.23.0
 
 pushd /tmp
-curl -O https://github.com/git/git/archive/${GIT_VERSION}.tar.gz
+curl -L -O https://github.com/git/git/archive/${GIT_VERSION}.tar.gz
 tar xf v2.23.0.tar.gz
 ls -lang
 
