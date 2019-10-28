2e199b8d05e7930df12a08a399fb19e94da79709
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-29 08:31:37 +0900

    Update git.sh

--- build_scripts/git.sh
+++ build_scripts/git.sh
@@ -6,6 +6,10 @@ date
 
 GIT_VERSION=2.23.0
 
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
 pushd /tmp
 curl -L -O https://github.com/git/git/archive/v${GIT_VERSION}.tar.gz
 tar xf v${GIT_VERSION}.tar.gz
@@ -15,7 +19,10 @@ make configure
 ./configure --help
 ./configure --prefix /tmp/usr
 time make -j2
+make install
 popd
 popd
+
+tree /tmp/usr
 
 date
