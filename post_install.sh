a925217a78cad99ca5f3e493c1025f3bec1790c4
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-29 08:56:16 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,6 +6,9 @@ date
 
 chmod +x ./start_web.sh
 
+GITHUB_USER_DECODE=$(echo -n ${GITHUB_USER} | base64 -d)
+GITHUB_PASSWORD_DECODE=$(echo -n ${GITHUB_PASSWORD} | base64 -d)
+
 pushd build_scripts
 chmod +x ./${BUILD_SCRIPT_NAME}.sh
 ./${BUILD_SCRIPT_NAME}.sh
