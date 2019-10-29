65a56a0913f7515246ad09fc784592b1b46d8869
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-29 09:01:17 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,6 +9,14 @@ chmod +x ./start_web.sh
 GITHUB_USER_DECODE=$(echo -n ${GITHUB_USER} | base64 -d)
 GITHUB_PASSWORD_DECODE=$(echo -n ${GITHUB_PASSWORD} | base64 -d)
 
+cat << __HEREDOC__ >> /app/.netrc
+machine github.com
+  login ${GITHUB_USER_DECODE}
+  password ${GITHUB_PASSWORD_DECODE}
+__HEREDOC__
+
+cat /app/.netrc
+
 pushd build_scripts
 chmod +x ./${BUILD_SCRIPT_NAME}.sh
 ./${BUILD_SCRIPT_NAME}.sh
