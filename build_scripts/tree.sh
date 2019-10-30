81eb0f9e2c15749f15a465d952e46fd01d3c4f02
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-30 10:07:11 +0900

    Create tree.sh

---
+++ build_scripts/tree.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+date
+
+TREE_VERSION=1.8.0
+
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+pushd /tmp
+
+curl -O http://mama.indstate.edu/users/ice/tree/src/tree-${TREE_VERSION}.tgz
+tar xf tree-${TREE_VERSION}.tgz
+ls -lang
+# pushd parallel-${PARALLEL_VERSION}
+# ./configure --help
+# time ./configure --prefix=/tmp/usr
+
+# time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
+# time make install
+# popd
+popd
+
+tree /tmp/usr
+
+# ldd /tmp/usr/bin/parallel
+
+# /tmp/usr/bin/parallel --version
+
+# cp /tmp/usr/bin/parallel ../www/
+
+date
