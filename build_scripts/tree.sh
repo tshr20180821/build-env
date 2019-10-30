dcfe11d026f7a7aeff55459cd5fd1e322b9e4c10
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-30 10:12:28 +0900

    Update tree.sh

--- build_scripts/tree.sh
+++ build_scripts/tree.sh
@@ -6,8 +6,8 @@ date
 
 TREE_VERSION=1.8.0
 
-export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
-export CXXFLAGS="$CFLAGS"
+# export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+# export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
 export PATH="/tmp/usr/bin:${PATH}"
@@ -18,11 +18,9 @@ curl -O http://mama.indstate.edu/users/ice/tree/src/tree-${TREE_VERSION}.tgz
 tar xf tree-${TREE_VERSION}.tgz
 ls -lang
 pushd tree-${TREE_VERSION}
-./configure --help
-time ./configure --prefix=/tmp/usr
 
 time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
-time make install
+ls -lang
 popd
 popd
 
