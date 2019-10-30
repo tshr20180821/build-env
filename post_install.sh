2997a1d566bf1afda07985f83e598235752795ea
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-30 10:40:12 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -59,7 +59,7 @@ curl -sS -o heroku/heroku.tar.gz https://cli-assets.heroku.com/heroku-cli/channe
 pushd heroku
 tar xf heroku.tar.gz --strip-components=1
 rm heroku.tar.gz
-timeout -sKILL 10 ./bin/heroku ps:exec --status
+timeout -sKILL 10 ./bin/heroku ps:exec --status -a ${HEROKU_APP_NAME}
 popd
 
 # ***** target *****
