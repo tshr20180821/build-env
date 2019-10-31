66baa898fcb83036adaeff22158ce19b8224c3bf
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-01 08:57:42 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -6,12 +6,6 @@ hostname -i
 echo ${PORT}
 whoami
 
-/usr/sbin/sshd -V
-
-ssh-keygen -t rsa -N '' -f etc/ssh_host_rsa_key
-
-/usr/sbin/sshd -d -p 60022 -f etc/sshd_config &
-
-sleep 10 && timeout -sKILL 10 ss -ant &
+# sleep 10 && timeout -sKILL 10 ss -ant &
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
