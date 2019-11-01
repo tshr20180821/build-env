37f7a3942216dc6d3888e8c7dd680d4ec92c591a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-01 10:09:16 +0900

    Update brotli.sh

--- build_scripts/brotli.sh
+++ build_scripts/brotli.sh
@@ -12,5 +12,8 @@ curl -L -O https://github.com/google/brotli/archive/v${BROTLI_VERSION}.tar.gz
 tar xf v${BROTLI_VERSION}.tar.gz
 pushd brotli-${BROTLI_VERSION}
 ls -lang
+./configure --help
+./configure --prefix=/tmp/usr
+time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
 popd
 popd
