3cabfbf2f6381173f8b52e37012c6bd63b0c44ee
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-01 10:07:54 +0900

    Update brotli.sh

--- build_scripts/brotli.sh
+++ build_scripts/brotli.sh
@@ -10,6 +10,7 @@ BROTLI_VERSION=1.0.7
 pushd /tmp
 curl -L -O https://github.com/google/brotli/archive/v${BROTLI_VERSION}.tar.gz
 tar xf v${BROTLI_VERSION}.tar.gz
+pushd brotli-${BROTLI_VERSION}
 ls -lang
-
+popd
 popd
