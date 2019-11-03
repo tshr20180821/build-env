1364f66ae25171e080ee9cfa8775adb1c0096634
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-03 09:07:10 +0900

    Create megatools.sh

---
+++ build_scripts/megatools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+date
+
+MEGATOOLS_VERSION=1.10.2
+
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+pushd /tmp
+
+curl -O https://megatools.megous.com/builds/megatools-${MEGATOOLS_VERSION}.tar.gz
+
+pushd megatools-${MEGATOOLS_VERSION}
+./configure --help
+./configure --prefix=/tmp/usr
+time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
+make install
+popd
+popd
+
+date
