e5b9950ccbd4d2f91f437d73216b3ee6fc9ae5d2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-07 10:09:32 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -13,8 +13,11 @@ timeout -sKILL 10 ss -t
 echo 'Processor Count : ' $(grep -c -e processor /proc/cpuinfo)
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
 
+cat /proc/version
+
 whereis gcc
 gcc --version
+ldd /usr/bin/gcc
 
 # ***** github auth & heroku auth *****
 
