05b0e883689ccbb83152fe2d0229eaba8e8de354
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-08 14:54:37 +0900

    Update curl.sh

--- build_scripts/curl.sh
+++ build_scripts/curl.sh
@@ -49,7 +49,7 @@ pushd curl-${CURL_VERSION}
 ./configure --help
 ./configure --prefix=/tmp/usr --enable-shared=no --enable-static=yes \
   --with-libssh2 --with-brotli --with-nghttp2 \
-  --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc
+  --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc --enable-esni
 
 time timeout -sKILL 210 make
 if [ $? != 0 ]; then
