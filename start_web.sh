9b8a21cfb8e77d6dc7a257b1a8dfc9e8fd43a445
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-13 10:06:43 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,8 +4,6 @@ set -x
 
 # ***** env ******
 
-export HEROKU_EXEC_DEBUG=1
-
 echo 'Processor Count : ' $(grep -c -e processor /proc/cpuinfo)
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
 
