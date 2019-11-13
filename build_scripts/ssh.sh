94b7ceb74668283d19e760a6537a670ccd0cdb0a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-13 11:38:39 +0900

    Update ssh.sh

--- build_scripts/ssh.sh
+++ build_scripts/ssh.sh
@@ -4,13 +4,16 @@ set -x
 
 date
 
-SSH_VERSION=8_1_P1
+# SSH_VERSION=8_1_P1
+SSH_VERSION=7_8_P1
 
 export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
 pushd /tmp
+
+curl -O https://jaist.dl.sourceforge.net/project/hpnssh/HPN-SSH%2014v16%207.8p1/openssh-7_8_P1-hpn-14.16.diff
 
 curl -L -O https://github.com/openssh/openssh-portable/archive/V_${SSH_VERSION}.tar.gz
 tar xf V_${SSH_VERSION}.tar.gz
