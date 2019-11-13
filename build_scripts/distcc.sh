6c318e295203beef7ae5c7416f25cf27b5c1df93
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-13 14:59:41 +0900

    Update distcc.sh

--- build_scripts/distcc.sh
+++ build_scripts/distcc.sh
@@ -9,7 +9,7 @@ DISTCC_VERSION=3.3.3
 
 whereis python
 python --version
-find / -name Python.h -print
+find / -name Python.h -print 2>/dev/null
 
 export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer -I../.apt/usr/include/python3.6m/"
 export CXXFLAGS="$CFLAGS"
