61b8b477eec3fc29b4d50f2e7bbc6cbdf0cd5eba
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 10:55:23 +0900

    Update pip.sh

--- build_scripts/pip.sh
+++ build_scripts/pip.sh
@@ -9,6 +9,23 @@ export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
+mkdir -m 777 /tmp/usr/bin
+
+cat << '__HEREDOC__' >/tmp/usr/bin/make
+#!/bin/bash
+
+set -x
+
+echo $@
+/usr/bin/make -j2 $@
+__HEREDOC__
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+chmod +x /tmp/usr/bin/make
+whereis make
+echo ${PATH}
+
 export PYTHONUSERBASE=/tmp/python
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 time python /tmp/get-pip.py --user
