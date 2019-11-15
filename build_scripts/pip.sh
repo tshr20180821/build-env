956532b17fd053a6b7b3d4b58e3678e9e9a83444
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 11:43:25 +0900

    Update pip.sh

--- build_scripts/pip.sh
+++ build_scripts/pip.sh
@@ -9,7 +9,26 @@ export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
-export MAKEFLAGS="-j2"
+export CCACHE_DIR=/tmp/ccache_cache
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+if [ -v TARGET_SSH_PORT ]; then
+  export PARALLEL_COUNT=9
+  export CCACHE_PREFIX="/tmp/bin/distcc"
+else
+  export PARALLEL_COUNT=2
+fi
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
+  
+ccache -s
+ccache -z
 
 export PYTHONUSERBASE=/tmp/python
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
@@ -21,8 +40,24 @@ time python /tmp/get-pip.py --user --no-warn-script-location
 # time /tmp/python/bin/pip install -I --user bzr mercurial
 time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user bzr
 
+ccache -s
+
 pushd ${PYTHONUSERBASE}
-tar cJf /tmp/pips.tar.xz ./
+time tar cJf /tmp/pips.tar.xz ./
+popd
+
+pushd /tmp
+time tar cf ccache_cache.tar.bz2 --use-compress-prog=lbzip2 ./ccache_cache
+mv ccache_cache.tar.bz2 repo/build-env/ccache_cache/
+pushd repo/build-env
+git init
+git config --global user.email "user"
+git config --global user.name "user"
+git add .
+git commit -a -m "."
+git remote set-url origin https://github.com/tshr20140816/build-env
+git push origin master
+popd
 popd
 
 date
