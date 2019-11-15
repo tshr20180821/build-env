8955f9842045857d915acfd246928589514ff7f7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 14:06:47 +0900

    Update pip.sh

--- build_scripts/pip.sh
+++ build_scripts/pip.sh
@@ -4,29 +4,20 @@ set -x
 
 date
 
-find / -name Python.h -print 2>/dev/null
-
-mkdir -m 777 -p /tmp/usr/include
-cp -r ${pwd}../.apt/usr/include/python3.6m /tmp/usr/include/
-tree /tmp/usr/include/
-
 cflags_option=$(cat /tmp/cflags_option)
-export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer -I/tmp/usr/include -I/tmp/usr -I/tmp/usr/include/python3.6m"
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
-
 
 export PYTHONUSERBASE=/tmp/python
 export PATH=${PYTHONUSERBASE}/bin:${PATH}
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-time python3.6 /tmp/get-pip.py --user
+time python /tmp/get-pip.py --user
 
-# pip --help
-# pip install --help
+pip --help
+pip install --help
 
-# time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user bzr mercurial
-time /tmp/python/bin/pip install -I --user bzr
-time /tmp/python/bin/pip install -I --user mercurial
+time /tmp/python/bin/pip install -v --no-color --progress-bar=ascii -I --user bzr mercurial
 
 pip freeze
 
