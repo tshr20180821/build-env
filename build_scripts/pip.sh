34eb03672c14aea4dd4ffed793d9bccdaa696bde
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 13:08:02 +0900

    Update pip.sh

--- build_scripts/pip.sh
+++ build_scripts/pip.sh
@@ -9,6 +9,10 @@ export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
+export PATH=/tmp/usr/bin:${PATH}
+mkdir -m 777 -p /tmp/usr/bin
+ln -s /usr/bin/python3.6 /tmp/usr/bin/python
+
 export PYTHONUSERBASE=/tmp/python
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 time python /tmp/get-pip.py --user --no-warn-script-location
@@ -16,7 +20,8 @@ time python /tmp/get-pip.py --user --no-warn-script-location
 /tmp/python/bin/pip --help
 /tmp/python/bin/pip install --help
 
-time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user bzr mercurial
+# time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user bzr mercurial
+time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user mercurial
 
 /tmp/python/bin/pip freeze
 
