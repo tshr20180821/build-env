6fdff0ebb6cf2fc51f628098d11d6883381b0c5e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 11:39:30 +0900

    Update pip.sh

--- build_scripts/pip.sh
+++ build_scripts/pip.sh
@@ -9,29 +9,7 @@ export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
-mkdir -m 777 /tmp/usr/bin
-
-ls /usr/bin | xargs -n 1 -I {} ln -s /usr/bin/{} /tmp/usr/bin/{}
-rm /tmp/usr/bin/make
-
-cat << '__HEREDOC__' >/tmp/usr/bin/make
-#!/bin/bash
-
-set -x
-
-echo $@
-/usr/bin/make -j2 $@
-__HEREDOC__
-
-chmod +x /tmp/usr/bin/make
-
-export PATH="/tmp/usr/bin:${PATH}"
-export PATH_OLD=${PATH}
-export PATH=$(echo ${PATH} | sed -e 's|:/usr/bin:|:|g')
-
-whereis make
-make --version
-echo ${PATH}
+export MAKEFLAGS="-j2"
 
 export PYTHONUSERBASE=/tmp/python
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
@@ -42,8 +20,5 @@ time python /tmp/get-pip.py --user --no-warn-script-location
 
 # time /tmp/python/bin/pip install -I --user bzr mercurial
 time /tmp/python/bin/pip install --no-color --progress-bar=ascii -I --user bzr
-
-rm /tmp/usr/bin/make
-export PATH=${PATH_OLD}
 
 date
