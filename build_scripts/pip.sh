047bac88d624b54eb722f7c942c20a5261f15b78
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-15 10:44:36 +0900

    Create pip.sh

---
+++ build_scripts/pip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+date
+
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+export PYTHONUSERBASE=/tmp/python
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+time python /tmp/get-pip.py --user
+# time /tmp/python/bin/pip install -I --user bzr mercurial
+time /tmp/python/bin/pip install -I --user bzr
+
+date
