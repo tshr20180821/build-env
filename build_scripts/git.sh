e3adbff7c1a56f41516c94469226c8cb44fad783
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-18 11:18:19 +0900

    Update git.sh

--- build_scripts/git.sh
+++ build_scripts/git.sh
@@ -6,13 +6,21 @@ date
 
 GIT_VERSION=2.23.0
 
-export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
 export CCACHE_DIR=/tmp/ccache_cache
 
 export PATH="/tmp/usr/bin:${PATH}"
+
+if [ -v TARGET_SSH_PORT ]; then
+  export PARALLEL_COUNT=9
+  export CCACHE_PREFIX="/tmp/bin/distcc"
+else
+  export PARALLEL_COUNT=2
+fi
 
 pushd /tmp/usr/bin
 ln -s ccache gcc
