991958902e5aca9e1c1bc3d428891629d5686aab
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-19 16:43:31 +0900

    Update newrelic.sh

--- build_scripts/newrelic.sh
+++ build_scripts/newrelic.sh
@@ -23,14 +23,17 @@ time dpkg --instdir=/tmp/newrelic --admindir=/tmp/newrelic --root=/tmp/newrelic 
   newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb \
   newrelic-php5_${NEWRELIC_VERSION}_amd64.deb
 
-mkdir repo
+mkdir repo2
 dpkg-deb --help
-dpkg-deb -x newrelic-php5-common_${NEWRELIC_VERSION}_all.deb repo
-dpkg-deb -x newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb repo
-dpkg-deb -x newrelic-php5_${NEWRELIC_VERSION}_amd64.deb repo
+dpkg-deb -x newrelic-php5-common_${NEWRELIC_VERSION}_all.deb repo2
+dpkg-deb -x newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb repo2
+dpkg-deb -x newrelic-php5_${NEWRELIC_VERSION}_amd64.deb repo2
+dpkg-deb -e newrelic-php5-common_${NEWRELIC_VERSION}_all.deb repo2
+dpkg-deb -e newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb repo2
+dpkg-deb -e newrelic-php5_${NEWRELIC_VERSION}_amd64.deb repo2
 
 ls -lang
-ls -lang repo
+ls -lang repo2
 
 popd
 
