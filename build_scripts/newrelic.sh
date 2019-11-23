2e4b9d5b54ad450bdb3e81aa2691c645595a2e0a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-11-24 08:07:27 +0900

    Update newrelic.sh

--- build_scripts/newrelic.sh
+++ build_scripts/newrelic.sh
@@ -7,13 +7,11 @@ date
 NEWRELIC_VERSION=9.3.0.248
 
 time curl -sSL -o /tmp/newrelic-php5-common_${NEWRELIC_VERSION}_all.deb \
-  https://download.newrelic.com/debian/dists/newrelic/non-free/binary-amd64/newrelic-php5-common_${NEWRELIC_VERSION}_all.deb &
+  https://download.newrelic.com/debian/dists/newrelic/non-free/binary-amd64/newrelic-php5-common_${NEWRELIC_VERSION}_all.deb
 time curl -sSL -o /tmp/newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb \
-  https://download.newrelic.com/debian/dists/newrelic/non-free/binary-amd64/newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb &
+  https://download.newrelic.com/debian/dists/newrelic/non-free/binary-amd64/newrelic-daemon_${NEWRELIC_VERSION}_amd64.deb
 time curl -sSL -o /tmp/newrelic-php5_${NEWRELIC_VERSION}_amd64.deb \
   https://download.newrelic.com/debian/dists/newrelic/non-free/binary-amd64/newrelic-php5_${NEWRELIC_VERSION}_amd64.deb
-
-wait
 
 dpkg-deb --help
 dpkg-deb -x /tmp/newrelic-php5-common_${NEWRELIC_VERSION}_all.deb .apt
