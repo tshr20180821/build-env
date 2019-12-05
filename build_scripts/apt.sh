d5ec25c85726e404dfeb426dc041c36bf72b3d4f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-05 17:32:08 +0900

    Update apt.sh

--- build_scripts/apt.sh
+++ build_scripts/apt.sh
@@ -10,11 +10,11 @@ APT_OPTIONS="-o debug::nolocking=true -o dir::cache=/tmp -o dir::state=/tmp"
 APT_FORCE_YES="-y --allow-downgrades --allow-remove-essential --allow-change-held-packages"
 
 apt-get ${APT_OPTIONS} update
-apt-get ${APT_OPTIONS} -s --print-uris upgrade
 apt-get ${APT_OPTIONS} -s -V upgrade | grep -o -E '^   [a-zA-Z0-9].+? ' | awk '{print $1}' >/tmp/update_list
 
 for PACKAGE in $(cat /tmp/update_list); do
-  time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -s -d --print-uris install --reinstall ${PACKAGE}
+  # time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -s -d --print-uris install --reinstall ${PACKAGE}
+  time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} install --reinstall --print-uris -qq ${PACKAGE}
 done
 
 # for DEB in $(ls -1 /tmp/archives/*.deb); do
