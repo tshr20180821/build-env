a68d65d95257af305c3712bf686e380bdf140e21
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-05 13:26:59 +0900

    Update apt.sh

--- build_scripts/apt.sh
+++ build_scripts/apt.sh
@@ -4,6 +4,16 @@ set -x
 
 date
 
+BUILD_DIR=$(pwd)
 
+APT_OPTIONS="-o debug::nolocking=true -o dir::cache=/tmp -o dir::state=/tmp"
+APT_FORCE_YES="-y --allow-downgrades --allow-remove-essential --allow-change-held-packages"
+
+apt-get ${APT_OPTIONS} update
+apt-get ${APT_OPTIONS} -s -V upgrade | grep -o -E '^   [a-zA-Z0-9].+? ' | awk '{print $1}' >/tmp/update_list
+
+# for PACKAGE in $(cat /tmp/update_list); do
+#   apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -d install --reinstall ${PACKAGE}
+# done
 
 date
