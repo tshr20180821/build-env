59f74a2a722fca43bfef8e45d8d55a49fb848467
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-05 17:20:26 +0900

    Update apt.sh

--- build_scripts/apt.sh
+++ build_scripts/apt.sh
@@ -13,11 +13,11 @@ apt-get ${APT_OPTIONS} update
 apt-get ${APT_OPTIONS} -s -V upgrade | grep -o -E '^   [a-zA-Z0-9].+? ' | awk '{print $1}' >/tmp/update_list
 
 for PACKAGE in $(cat /tmp/update_list); do
-  apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -d install --reinstall ${PACKAGE}
+  time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -d install --reinstall ${PACKAGE}
 done
 
-# for DEB in $(ls -1 /tmp/archives/*.deb); do
-#   dpkg -x ${DEB} ${BUILD_DIR}/.apt/
-# done
+for DEB in $(ls -1 /tmp/archives/*.deb); do
+  time dpkg -x ${DEB} ${BUILD_DIR}/.apt/
+done
 
 date
