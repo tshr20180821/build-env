4fdb9fd4045d86014e34e48a739ce28fc0bf8fe9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-05 17:26:40 +0900

    Update apt.sh

--- build_scripts/apt.sh
+++ build_scripts/apt.sh
@@ -10,6 +10,7 @@ APT_OPTIONS="-o debug::nolocking=true -o dir::cache=/tmp -o dir::state=/tmp"
 APT_FORCE_YES="-y --allow-downgrades --allow-remove-essential --allow-change-held-packages"
 
 apt-get ${APT_OPTIONS} update
+apt-get ${APT_OPTIONS} -s --print-uris upgrade
 apt-get ${APT_OPTIONS} -s -V upgrade | grep -o -E '^   [a-zA-Z0-9].+? ' | awk '{print $1}' >/tmp/update_list
 
 for PACKAGE in $(cat /tmp/update_list); do
