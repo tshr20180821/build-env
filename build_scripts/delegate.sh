fcf8e03f6a240a8a99f37ed0d858c29a2d7e005b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-06 12:42:40 +0900

    Update delegate.sh

--- build_scripts/delegate.sh
+++ build_scripts/delegate.sh
@@ -21,7 +21,7 @@ time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -d install --reinstall $(paste -s /
 ls -lang /tmp/archives/*.deb
 
 for DEB in $(ls -1 /tmp/archives/*.deb); do
-  time dpkg -x ${DEB} ${BUILD_DIR}/../.apt/
+  dpkg -x ${DEB} ${BUILD_DIR}/../.apt/
 done
 
 # ***** delegate *****
