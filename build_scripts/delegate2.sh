a658ac640390d22ec879c5d79325132c7ebe3123
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-11 17:16:26 +0900

    Update delegate2.sh

--- build_scripts/delegate2.sh
+++ build_scripts/delegate2.sh
@@ -29,7 +29,9 @@ done
 
 # ***** delegate *****
 
-export CFLAGS="-O2 -march=native -L/lib/x86_64-linux-gnu -L${BUILD_DIR}/../../app/.apt/usr/i686-linux-gnu/lib"
+ls ${BUILD_DIR}/../app/.apt/usr/i686-linux-gnu/lib
+
+export CFLAGS="-O2 -march=native -L/lib/x86_64-linux-gnu -L${BUILD_DIR}/../app/.apt/usr/i686-linux-gnu/lib"
 export CXXFLAGS="$CFLAGS"
 
 pushd /tmp
