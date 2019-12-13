beaa592fd806d487646fb84e432d3320da91c980
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-13 10:13:26 +0900

    Update delegate2.sh

--- build_scripts/delegate2.sh
+++ build_scripts/delegate2.sh
@@ -8,34 +8,9 @@ date
 
 BUILD_DIR=$(pwd)
 
-# APT_OPTIONS="-o debug::nolocking=true -o dir::cache=/tmp -o dir::state=/tmp"
-# APT_FORCE_YES="-y --allow-remove-essential --allow-change-held-packages"
-
-# apt-get ${APT_OPTIONS} update
-# echo libssl hold | dpkg --set-selections
-# apt-get ${APT_OPTIONS} -s -V upgrade | grep -o -E '^   [a-zA-Z0-9].+? ' | awk '{print $1}' >/tmp/update_list
-
-# echo "gcc-4.8" >>/tmp/update_list
-# echo "g++-4.8" >>/tmp/update_list
-# echo "gcc-5" >>/tmp/update_list
-# echo "g++-5" >>/tmp/update_list
-# echo "gcc-8" >>/tmp/update_list
-# echo "g++-8" >>/tmp/update_list
-
-# cat /tmp/update_list
-
-# time apt-get ${APT_OPTIONS} ${APT_FORCE_YES} -d install --reinstall $(paste -s /tmp/update_list)
-
-# ls -lang /tmp/archives/*.deb
-
-# for DEB in $(ls -1 /tmp/archives/*.deb); do
-#   dpkg -x ${DEB} ${BUILD_DIR}/../.apt/
-# done
-
 # ***** delegate *****
 
 export CFLAGS="-O2 -march=native -std=gnu++98"
-# export CFLAGS="-O2 -march=native"
 export CXXFLAGS="$CFLAGS"
 
 pushd /tmp
@@ -51,9 +26,6 @@ echo "<HR>" >./src/builtin/mssgs/news/artlistfooter.dhtml
 diff ${BUILD_DIR}/../files/fpoll.h include/fpoll.h
 cp -f ${BUILD_DIR}/../files/fpoll.h include/
 
-# time make ADMIN="admin@localhost" CC=gcc-4.8 CXX=g++-4.8
-# time make ADMIN="admin@localhost" CC=gcc-5 CXX=g++-5
-# time make ADMIN="admin@localhost" CC=gcc-8 CXX=g++-8
 time make ADMIN="admin@localhost"
 
 ldd ./src/delegated
