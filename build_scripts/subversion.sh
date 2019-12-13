cc80fd49c85e01d3916cc05ac3f94b1ba76e9cec
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-13 16:38:27 +0900

    Update subversion.sh

--- build_scripts/subversion.sh
+++ build_scripts/subversion.sh
@@ -10,9 +10,32 @@ SUBVERSION_VERSION=1.13.0
 
 ls -lang ${BUILD_DIR}/../.apt/usr/lib/x86_64-linux-gnu
 
-export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer -L${BUILD_DIR}/../.apt/usr/lib/x86_64-linux-gnu"
+# export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer -L${BUILD_DIR}/../.apt/usr/lib/x86_64-linux-gnu"
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
+
+export CCACHE_DIR=/tmp/ccache_cache
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+if [ -v TARGET_SSH_PORT ]; then
+  export PARALLEL_COUNT=9
+  export CCACHE_PREFIX="/tmp/bin/distcc"
+else
+  export PARALLEL_COUNT=2
+fi
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
+
+ccache -s
+ccache -z
 
 pushd /tmp
 
@@ -24,7 +47,26 @@ pushd subversion-${SUBVERSION_VERSION}
 ./configure --help
 time ./configure --prefix=/tmp/usr --enable-shared=no
 time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
-make install
+# make install
+popd
+popd
+
+ccache -s
+
+pushd /tmp
+time tar cf ccache_cache.tar.bz2 --use-compress-prog=lbzip2 ./ccache_cache
+ls -lang ccache_cache.tar.bz2
+mv ccache_cache.tar.bz2 repo/build-env/ccache_cache/
+pushd repo/build-env
+git init
+git config --global user.email "user"
+git config --global user.name "user"
+# 1MB -> 30MB
+# git config --global http.postbuffer 31457280
+git add .
+git commit -a -m "."
+git remote set-url origin https://github.com/tshr20140816/build-env
+time git push origin master
 popd
 popd
 
