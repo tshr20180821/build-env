f551cfac64b572cc63293281c562c195354885aa
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-13 16:09:13 +0900

    Create subversion.sh

---
+++ build_scripts/subversion.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+date
+
+SUBVERSION_VERSION=1.13.0
+
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+pushd /tmp
+
+time curl -O http://ftp.riken.jp/net/apache/subversion/subversion-1.13.0.tar.bz2
+
+tar xf subversion-1.13.0.tar.bz2
+
+pushd subversion-1.13.0.tar.bz2
+./configure --help
+time ./configure --prefix=/tmp/usr
+time make
+make install
+popd
+popd
+
+tree /tmp/usr
+
+ldd /tmp/usr/bin/svn
+/tmp/usr/bin/svn --version
+
+cp /tmp/usr/bin/svn ../www/
