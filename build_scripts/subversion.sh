7eb5be1410ce592ca90a6dd42e32acfe2a83be2c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-13 16:31:07 +0900

    Update subversion.sh

--- build_scripts/subversion.sh
+++ build_scripts/subversion.sh
@@ -4,19 +4,23 @@ set -x
 
 date
 
+BUILD_DIR=$(pwd)
+
 SUBVERSION_VERSION=1.13.0
 
-export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+ls -lang ${BUILD_DIR}/../.apt/usr/lib/x86_64-linux-gnu
+
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer -L${BUILD_DIR}/../.apt/usr/lib/x86_64-linux-gnu"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
 pushd /tmp
 
-time curl -O http://ftp.riken.jp/net/apache/subversion/subversion-1.13.0.tar.bz2
+time curl -O http://ftp.riken.jp/net/apache/subversion/subversion-${SUBVERSION_VERSION}.tar.bz2
 
-tar xf subversion-1.13.0.tar.bz2
+tar xf subversion-${SUBVERSION_VERSION}.tar.bz2
 
-pushd subversion-1.13.0
+pushd subversion-${SUBVERSION_VERSION}
 ./configure --help
 time ./configure --prefix=/tmp/usr --enable-shared=no
 time timeout -sKILL 210 make -j$(grep -c -e processor /proc/cpuinfo)
