e143034c354bb3db392005b480ab252bc954a18f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-18 12:03:54 +0900

    Update subversion.sh

--- build_scripts/subversion.sh
+++ build_scripts/subversion.sh
@@ -76,7 +76,7 @@ git config --global http.postbuffer 31457280
 git add .
 git commit -a -m "."
 git remote set-url origin https://github.com/tshr20140816/build-env
-time git push origin master
+# time git push origin master
 popd
 popd
 
