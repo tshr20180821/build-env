9819c9302ff8afcfd9b9318c0d1446ce07045b94
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-18 09:01:00 +0900

    Update subversion.sh

--- build_scripts/subversion.sh
+++ build_scripts/subversion.sh
@@ -22,7 +22,7 @@ export CCACHE_DIR=/tmp/ccache_cache
 
 export PATH="/tmp/usr/bin:${PATH}"
 
-mkdir /tmp/usr/bin
+mkdir -p /tmp/usr/bin
 cp ../bin/ccache /tmp/usr/bin/
 
 # if [ -v TARGET_SSH_PORT ]; then
