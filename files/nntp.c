ace217f95f0d9fc388a228821f41e5e48fc5ab1a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-12-25 10:37:57 +0900

    Update nntp.c

--- files/nntp.c
+++ files/nntp.c
@@ -5625,7 +5625,7 @@ int NNTP_newServer(Connection *Conn,PCStr(proto),PCStr(user),PCStr(pass),PCStr(h
 	sv1log("FQDN: %s\n",hostFQDN);
 
 	if( strcaseeq(proto,"pop") && port == 110 ){
-		port = 6110;
+		port = 60110;
 	}
 
 	nsx = allocServer();
