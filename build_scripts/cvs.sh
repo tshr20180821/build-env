85ae6492de33740e80a35f3d920f85137ae44b8e
tshr20140816 <tshr20140816@yahoo.co.jp>
2020-01-08 11:20:35 +0900

    Update cvs.sh

--- build_scripts/cvs.sh
+++ build_scripts/cvs.sh
@@ -9,9 +9,9 @@ gcc -v --help 2>/dev/null | grep -E "^\s+\-std=.*$"
 CVS_VERSION=1.11.23
 
 # export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer -std=c99"
-export CFLAGS="-std=c99 -Wimplicit-function-declaration"
-export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-fuse-ld=gold"
+export CFLAGS="-std=c99"
+# export CXXFLAGS="$CFLAGS"
+# export LDFLAGS="-fuse-ld=gold"
 
 pushd /tmp
 
