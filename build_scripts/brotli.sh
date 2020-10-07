d2754a95df9dc57f976777e64fa2eb1e29d30aed
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-10-07 09:39:14 +0900

    Update brotli.sh

--- build_scripts/brotli.sh
+++ build_scripts/brotli.sh
@@ -16,7 +16,7 @@ pushd /tmp
 curl -L -O https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
 mkdir usr
 tar xf cmake-3.12.0-Linux-x86_64.tar.gz -C ./usr --strip=1
-tree ./usr
+# tree ./usr
 export PATH="/tmp/usr/bin:${PATH}"
 
 curl -L -O https://github.com/google/brotli/archive/v${BROTLI_VERSION}.tar.gz
