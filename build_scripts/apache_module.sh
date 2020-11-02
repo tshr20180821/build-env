10132fb1742bfc9bf9031409b9e03bd62954dee0
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-02 13:46:55 +0900

    Update apache_module.sh

--- build_scripts/apache_module.sh
+++ build_scripts/apache_module.sh
@@ -45,7 +45,8 @@ pushd /tmp
 wget https://github.com/c-ares/c-ares/releases/download/cares-1_16_1/c-ares-1.16.1.tar.gz &
 # wget http://www.digip.org/jansson/releases/jansson-2.11.tar.bz2 &
 wget http://www.digip.org/jansson/releases/jansson-2.13.1.tar.bz2 &
-wget https://github.com/nghttp2/nghttp2/releases/download/v1.32.0/nghttp2-1.32.0.tar.xz &
+# wget https://github.com/nghttp2/nghttp2/releases/download/v1.32.0/nghttp2-1.32.0.tar.xz &
+wget https://github.com/nghttp2/nghttp2/releases/download/v1.41.0/nghttp2-1.41.0.tar.xz &
 wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz &
 git clone --depth 1 https://github.com/google/brotli &
 wget http://ftp.jaist.ac.jp/pub/apache//apr/apr-1.6.3.tar.bz2 &
@@ -92,6 +93,25 @@ make install &
 
 popd
 
+# ***** nghttp2 *****
+
+# 1.41.0
+tar xf nghttp2-1.41.0.tar.xz
+target=nghttp2-1.41.0
+pushd ${target}
+
+./configure --help
+if [ -f ${BUILD_DIR}/../ccache_cache/config.cache.nghttp2 ]; then
+  ./configure --prefix=/tmp/usr --cache-file=${BUILD_DIR}/../ccache_cache/config.cache.nghttp2
+else
+  ./configure --prefix=/tmp/usr --config-cache
+  cp ./config.cache /tmp/config.cache.nghttp2
+fi
+time timeout -sKILL 90 make -j${PARALLEL_COUNT}
+make install &
+
+popd
+
 popd
 
 ccache -s
@@ -117,5 +137,6 @@ tree /tmp/usr
 
 cp /tmp/config.cache.c-ares ../www/
 cp /tmp/config.cache.jansson ../www/
+cp /tmp/config.cache.nghttp2 ../www/
 
 date
