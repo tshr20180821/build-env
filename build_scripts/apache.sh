2ddb002b65ee3d6990b0e91287bfd228d95703fb
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-02 15:17:06 +0900

    Create apache.sh

---
+++ build_scripts/apache.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+
+date
+
+APACHE_VERSION=2.4.46
+
+# - Aptfile -
+
+printenv | sort
+
+BUILD_DIR=$(pwd)
+
+# export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+export CCACHE_DIR=/tmp/ccache_cache
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+if [ -v TARGET_SSH_PORT ]; then
+  export PARALLEL_COUNT=9
+  export CCACHE_PREFIX="/tmp/bin/distcc"
+else
+  export PARALLEL_COUNT=2
+fi
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
+  
+ccache -s
+ccache -z
+
+pushd /tmp
+
+wget https://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.46.tar.bz2
+
+tar xf httpd-2.4.46.tar.bz2
+ls -lang
+
+popd
