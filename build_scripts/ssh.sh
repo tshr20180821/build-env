4c2e12f8121ad575676eeef1908e711ab0596103
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-18 14:10:19 +0900

    Update ssh.sh

--- build_scripts/ssh.sh
+++ build_scripts/ssh.sh
@@ -6,7 +6,8 @@ date
 
 # SSH_VERSION=7_8_P1
 # SSH_VERSION=8_1_P1
-SSH_VERSION=8_3_P1
+# SSH_VERSION=8_3_P1
+SSH_VERSION=8_4_P1
 
 export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
@@ -20,13 +21,16 @@ pushd openssh-portable-V_${SSH_VERSION}
 
 # curl -O https://ayera.dl.sourceforge.net/project/hpnssh/OpenSSL-1.1%20Compatibility/hpn-openssl1.1-7_8_P1.diff
 # curl -O https://excellmedia.dl.sourceforge.net/project/hpnssh/Patches/HPN-SSH%2014v20%208.1p1/openssh-8_1_P1-hpn-14.20.diff
-curl -O https://master.dl.sourceforge.net/project/hpnssh/Patches/HPN-SSH%2014v22%208.3p1/openssh-8_3_P1-hpn-14.22.diff
+# curl -O https://master.dl.sourceforge.net/project/hpnssh/Patches/HPN-SSH%2014v22%208.3p1/openssh-8_3_P1-hpn-14.22.diff
+# curl -O https://master.dl.sourceforge.net/project/hpnssh/Patches/HPN-SSH%2014v22%208.3p1/openssh-8_3_P1-hpn-14.22.diff
+curl -O https://master.dl.sourceforge.net/project/hpnssh/Patches/HPN-SSH%2015v1%208.4p1/openssh-8_4_P1-hpn-15.1.diff
 
 ls -lang
 
 # patch -p1 <./hpn-openssl1.1-7_8_P1.diff
 # patch -p1 <./openssh-8_1_P1-hpn-14.20.diff
-patch -p1 <./openssh-8_3_P1-hpn-14.22.diff
+# patch -p1 <./openssh-8_3_P1-hpn-14.22.diff
+patch -p1 <./openssh-8_4_P1-hpn-15.1.diff
 
 autoreconf
 ./configure --help
