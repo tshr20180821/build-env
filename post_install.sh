4cb5d8aa866132537deb86a150279c9ecffad6c2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-18 15:55:25 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -195,7 +195,7 @@ chmod +x distcc-ssh
 cat distcc-ssh
 popd
 
-export DISTCC_HOSTS="@0.0.0.0/8:/app/bin/distccd_start localhost/1"
+export DISTCC_HOSTS="localhost/1 @0.0.0.0/8:/app/bin/distccd_start"
 export DISTCC_POTENTIAL_HOSTS=${DISTCC_HOSTS}
 export DISTCC_SSH="/tmp/bin/distcc-ssh"
 
