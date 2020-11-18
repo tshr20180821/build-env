cdd284ab9c8f2508744844b6b7f8b54fe07fbd66
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-18 14:43:36 +0900

    Update curl.sh

--- build_scripts/curl.sh
+++ build_scripts/curl.sh
@@ -71,7 +71,7 @@ pushd curl-${CURL_VERSION}
 ./configure --help
 ./configure --prefix=/tmp/usr --enable-shared=no --enable-static=yes \
   --with-libssh2 --with-brotli --with-nghttp2 \
-  --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc --without-zstd
+  --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc ---enable-ech -without-zstd
 
 # time timeout -sKILL 210 make
 time timeout -sKILL 180 make -j${PARALLEL_COUNT}
