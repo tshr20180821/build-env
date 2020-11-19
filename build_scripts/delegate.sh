119407ac94bbcfd79dc4e3f7c982124b953e78a6
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 06:40:51 +0900

    Update delegate.sh

--- build_scripts/delegate.sh
+++ build_scripts/delegate.sh
@@ -10,12 +10,7 @@ BUILD_DIR=$(pwd)
 
 rm -f /tmp/update_list
 
-cat /etc/apt/sources.list >/tmp/sources.list
-
-echo "deb http://archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse" >>/tmp/sources.list
-echo "deb http://archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse" >>/tmp/sources.list
-echo "deb http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse" >>/tmp/sources.list
-echo "deb http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse" >>/tmp/sources.list
+cat /etc/apt/sources.list etc/apt.sources.list >/tmp/sources.list
 
 APT_OPTIONS="-o debug::nolocking=true -o dir::cache=/tmp -o dir::state=/tmp -o dir::etc::sourcelist=/tmp/sources.list"
 APT_FORCE_YES="-y --allow-remove-essential --allow-change-held-packages"
