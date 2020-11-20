342fc27691f3105fe534e2f6e06c51ff529e0ad8
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 09:25:56 +0900

    Update openssl.sh

--- build_scripts/openssl.sh
+++ build_scripts/openssl.sh
@@ -4,6 +4,32 @@ set -x
 
 date
 
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="-fuse-ld=gold"
+
+export CCACHE_DIR=/tmp/ccache_cache
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+if [ -v TARGET_SSH_PORT ]; then
+  export PARALLEL_COUNT=9
+  export CCACHE_PREFIX="/tmp/bin/distcc"
+else
+  export PARALLEL_COUNT=2
+fi
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
+  
+ccache -s
+ccache -z
+
 pushd /tmp
 
 time git clone --depth=1 -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl.git
@@ -11,7 +37,27 @@ time git clone --depth=1 -b OpenSSL_1_0_2-stable https://github.com/openssl/open
 pushd openssl
 time ./config -fPIC shared
 
-time make -j2
+time make -j${PARALLEL_COUNT}
 
 popd
 popd
+
+ccache -s
+
+pushd /tmp
+time tar cf ccache_cache.tar.bz2 --use-compress-prog=lbzip2 ./ccache_cache
+ls -lang ccache_cache.tar.bz2
+mv ccache_cache.tar.bz2 repo/build-env/ccache_cache/
+pushd repo/build-env
+git init
+git config --global user.email "user"
+git config --global user.name "user"
+# 1MB -> 30MB
+# git config --global http.postbuffer 31457280
+git add .
+git commit -a -m "."
+git remote set-url origin https://github.com/tshr20180821/build-env
+time git push origin master
+popd
+popd
+
