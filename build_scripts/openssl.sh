6d9ac6444bd620fcdb44abca4c125960404e22d1
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 09:53:01 +0900

    Update openssl.sh

--- build_scripts/openssl.sh
+++ build_scripts/openssl.sh
@@ -9,11 +9,13 @@ pushd /tmp
 time git clone --depth=1 -b OpenSSL_1_0_2u https://github.com/openssl/openssl.git
 
 pushd openssl
-time ./config -fPIC shared
+# time ./config -fPIC shared
+./config --prefix=/tmp/usr shared zlib
 
 time timeout -sKILL 180 make -j2
+make install
 
-tree
+tree /tmp/usr
 
 popd
 popd
