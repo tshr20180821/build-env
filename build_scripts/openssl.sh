cf8eea52096de70257d4b27a3a9d54c76c7cc841
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 09:22:55 +0900

    Update openssl.sh

--- build_scripts/openssl.sh
+++ build_scripts/openssl.sh
@@ -8,8 +8,10 @@ pushd /tmp
 
 time git clone --depth=1 -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl.git
 
-./config -fPIC shared
+pushd openssl
+time ./config -fPIC shared
 
 time make -j2
 
+popd
 popd
