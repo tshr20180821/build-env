bcba3fba93cb0ff347962e585229a98d8905b35f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 10:01:28 +0900

    Update openssl.sh

--- build_scripts/openssl.sh
+++ build_scripts/openssl.sh
@@ -10,12 +10,15 @@ time git clone --depth=1 -b OpenSSL_1_0_2u https://github.com/openssl/openssl.gi
 
 pushd openssl
 # time ./config -fPIC shared
-./config --prefix=/tmp/usr shared zlib
+./config --prefix=/tmp/usr shared
 
 time timeout -sKILL 180 make -j2
 make install
 
+popd
+popd
+
 tree /tmp/usr
 
-popd
-popd
+strings /tmp/usr/lib/libcrypto.so.1.0.0
+strings /tmp/usr/lib/libssl.so.1.0.0
