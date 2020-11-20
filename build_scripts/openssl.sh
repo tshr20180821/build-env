ebcf21f1eccfa76542c48d21762c8c3ff67788df
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-11-20 10:03:07 +0900

    Update openssl.sh

--- build_scripts/openssl.sh
+++ build_scripts/openssl.sh
@@ -4,6 +4,20 @@ set -x
 
 date
 
+export CCACHE_DIR=/tmp/ccache_cache
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
+  
+ccache -s
+ccache -z
+
 pushd /tmp
 
 time git clone --depth=1 -b OpenSSL_1_0_2u https://github.com/openssl/openssl.git
@@ -15,6 +29,25 @@ pushd openssl
 time timeout -sKILL 180 make -j2
 make install
 
+popd
+popd
+
+ccache -s
+
+pushd /tmp
+time tar cf ccache_cache.tar.bz2 --use-compress-prog=lbzip2 ./ccache_cache
+ls -lang ccache_cache.tar.bz2
+mv ccache_cache.tar.bz2 repo/build-env/ccache_cache/
+pushd repo/build-env
+git init
+git config --global user.email "user"
+git config --global user.name "user"
+# 1MB -> 30MB
+# git config --global http.postbuffer 31457280
+git add .
+git commit -a -m "."
+git remote set-url origin https://github.com/tshr20180821/build-env
+time git push origin master
 popd
 popd
 
