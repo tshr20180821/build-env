aee0849e04367ad17e2189ec87ce38009ff2cd74
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2020-12-24 00:57:36 +0900

    Update curl.sh

--- build_scripts/curl.sh
+++ build_scripts/curl.sh
@@ -89,6 +89,7 @@ ccache -s
 pushd /tmp
 time tar cf ccache_cache.tar.bz2 --use-compress-prog=lbzip2 ./ccache_cache
 ls -lang ccache_cache.tar.bz2
+du -h ccache_cache.tar.bz2
 mv ccache_cache.tar.bz2 repo/build-env/ccache_cache/
 pushd repo/build-env
 git init
