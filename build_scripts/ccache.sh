dab7e5799262a01a74dae9e351cff44100a696e5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-06-14 13:31:30 +0900

    Update ccache.sh

--- build_scripts/ccache.sh
+++ build_scripts/ccache.sh
@@ -6,7 +6,7 @@ date
 
 # CCACHE_VERSION=3.7.12
 # CCACHE_VERSION=4.0
-CCACHE_VERSION=4.2
+CCACHE_VERSION=4.3
 
 # - Aptfile -
 # libzstd-dev
@@ -27,12 +27,14 @@ rm /tmp/usr/bin/ccache
 pushd /tmp
 
 # wget https://cmake.org/files/v3.18/cmake-3.18.4-Linux-x86_64.tar.gz
-wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6-Linux-x86_64.tar.gz
+# wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6-Linux-x86_64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v3.20.3/cmake-3.20.3-linux-x86_64.tar.gz
 time git clone -b v${CCACHE_VERSION} --depth=1 https://github.com/ccache/ccache.git
 
 mkdir /tmp/usr
 # tar xf cmake-3.18.4-Linux-x86_64.tar.gz -C /tmp/usr --strip=1
-tar xf cmake-3.19.6-Linux-x86_64.tar.gz -C /tmp/usr --strip=1
+# tar xf cmake-3.19.6-Linux-x86_64.tar.gz -C /tmp/usr --strip=1
+tar xf cmake-3.20.3-Linux-x86_64.tar.gz -C /tmp/usr --strip=1
 
 export PATH="/tmp/usr/bin:${PATH}"
 
