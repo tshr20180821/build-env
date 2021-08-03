35d46b758c9dfbbd2e376e4955cd0fee812ae1cf
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-08-03 12:28:43 +0900

    Update curl.sh

--- build_scripts/curl.sh
+++ build_scripts/curl.sh
@@ -24,7 +24,7 @@ echo "libnghttp2-14" >>/tmp/update_list
 echo "libnghttp2-dev" >>/tmp/update_list
 echo "libpsl5" >>/tmp/update_list
 echo "libpsl-dev" >>/tmp/update_list
-# echo "libzstd-dev" >>/tmp/update_list
+echo "libzstd-dev" >>/tmp/update_list
 # echo "zstd" >>/tmp/update_list
 bash ../apt_install.sh
 
