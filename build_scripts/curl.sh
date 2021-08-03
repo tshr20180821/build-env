43730cde5c468608347bdcbdcf881a0c0588ab54
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-08-03 12:27:07 +0900

    Update curl.sh

--- build_scripts/curl.sh
+++ build_scripts/curl.sh
@@ -65,7 +65,7 @@ pushd curl-${CURL_VERSION}
 ./configure --help
 ./configure --prefix=/tmp/usr --enable-shared=no --enable-static=yes \
   --with-libssh2 --with-brotli --with-nghttp2 --with-openssl \
-  --with-gssapi --enable-alt-svc # --without-zstd
+  --with-gssapi --enable-alt-svc --with-zstd
 #   --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc --without-zstd
 #   --with-gssapi --with-libmetalink=/tmp/usr --enable-alt-svc
 
