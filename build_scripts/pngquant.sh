1dcdef16940039146bc6dc6358e9c09698539849
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2022-01-05 06:47:23 +0900

    Update pngquant.sh

--- build_scripts/pngquant.sh
+++ build_scripts/pngquant.sh
@@ -7,7 +7,8 @@ date
 # PNGQUANT_VERSION=2.12.5
 # PNGQUANT_VERSION=2.13.1
 # PNGQUANT_VERSION=2.15.1
-PNGQUANT_VERSION=2.16.0
+# PNGQUANT_VERSION=2.16.0
+PNGQUANT_VERSION=2.17.0
 
 # export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 # export CXXFLAGS="$CFLAGS"
