cce4c4a54492efc838774216d53eccd03847310c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2022-05-05 04:38:44 +0900

    Update subversion.sh

--- build_scripts/subversion.sh
+++ build_scripts/subversion.sh
@@ -38,8 +38,9 @@ export CCACHE_DIR=/tmp/ccache_cache
 export PATH="/tmp/usr/bin:${PATH}"
 
 if [ -v TARGET_SSH_PORT ]; then
-  export PARALLEL_COUNT=9
-  export CCACHE_PREFIX="/tmp/bin/distcc"
+  export PARALLEL_COUNT=2
+  # export PARALLEL_COUNT=9
+  # export CCACHE_PREFIX="/tmp/bin/distcc"
 else
   export PARALLEL_COUNT=2
 fi
